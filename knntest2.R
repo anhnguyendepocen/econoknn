80f3290fbe9001e0345c35916cfaef0cc89dd2e7
James Rising <jarising@gmail.com>
2017-09-20 09:12:11 -0500

    move to office

--- knntest2.R
+++ knntest2.R
@@ -6,7 +6,8 @@ library(readstata13)
 
 df <- read.dta13("/home/solomon/Dropbox/GCP_Reanalysis/interpolation/data/consolidated/mortality_AEA_time-variant-inc_2.dta")
 df <- subset(df, agegrp == 33)
+df <- subset(df, !is.na(precip1_GMFD) & !is.na(precip2_GMFD))
 
 factorouts <- c('precip1_GMFD : factor(adm1)', 'precip2_GMFD : factor(adm1)', 'factor(adm2)', 'factor(adm1) : factor(year)')
 
-save.demean(outfile, df, 'deathrate', 'GMFD_poly1', factorouts)
+save.demean("time-variant.RData", df, 'deathrate', 'GMFD_poly1', factorouts)
